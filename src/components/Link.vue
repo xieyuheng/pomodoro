aa38bf9a6df6c8af8c06975a573b22976eb1e581
Xie Yuheng <xyheme@gmail.com>
2022-08-04 21:12:10 +0800

    up

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,10 +1,13 @@
-<script setup>
-// Maybe we can not use <RouterLink> in headlessui:
-// - https://headlessui.com/react/menu#integrating-with-next-js
+<script setup lang="ts">
+// We can not use <RouterLink> in headlessui:
+// - https://github.com/tailwindlabs/headlessui/issues/143
 </script>
 
 <template>
-  <RouterLink :to="$attrs.href" v-bind="$attrs">
-    <slot />
-  </RouterLink>
+  <span
+    @click="
+      typeof $attrs?.href === 'string' && $router.push({ path: $attrs.href })
+    "
+    ><a><slot /></a
+  ></span>
 </template>
