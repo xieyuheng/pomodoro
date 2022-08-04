c94b60813abca4c8c4f7b04e610ce6a36022e463
Xie Yuheng <xyheme@gmail.com>
2022-08-04 17:25:33 +0800

    up

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -2,4 +2,6 @@ import type { RouteRecordRaw } from "vue-router"
 
 export const routes: Array<RouteRecordRaw> = [
   { path: "/", component: () => import("./pomodoro/Pomodoro.vue") },
+  { path: "/login", component: () => import("./login/Login.vue") },
+  { path: "/register", component: () => import("./register/Register.vue") },
 ]
