f8242938e78074a5ac455926b092517c930e2d29
Xie Yuheng <xyheme@gmail.com>
2022-08-04 13:34:03 +0800

    up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <div class="text-3xl text-red-400">
+  <div class="p-4 text-3xl text-red-400">
     <h1>{{ msg }}</h1>
     <button @click="count++">count: {{ count }}</button>
   </div>
