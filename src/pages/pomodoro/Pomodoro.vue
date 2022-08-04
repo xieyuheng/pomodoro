c712f14a4a54395c2214fab56c4b9b448e4d2cf1
Xie Yuheng <xyheme@gmail.com>
2022-08-04 16:49:11 +0800

    up

--- src/pages/pomodoro/Pomodoro.vue
+++ src/pages/pomodoro/Pomodoro.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, reactive } from "vue"
 import { Head } from "@vueuse/head"
+import PageLayout from "../../layouts/page-layout/PageLayout.vue"
 import PomodoroTimer from "./PomodoroTimer.vue"
 import PomodoroTasks from "./PomodoroTasks.vue"
 import { PomodoroState as State } from "./PomodoroState"
@@ -11,13 +12,13 @@ onMounted(() => state.setupNotification())
 </script>
 
 <template>
-  <div>
-    <head>
-      <title>{{ state.formatTitle() }}</title>
-      <meta name="theme-color" :content="state.theme.color" />
-    </head>
+  <head>
+    <title>{{ state.formatTitle() }}</title>
+    <meta name="theme-color" :content="state.theme.color" />
+  </head>
 
+  <PageLayout>
     <PomodoroTimer :state="state" />
     <PomodoroTasks :state="state" />
-  </div>
+  </PageLayout>
 </template>
