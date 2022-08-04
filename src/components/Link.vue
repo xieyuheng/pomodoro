65eaff092b9247fd91e52125946b57a9c6b2b708
Xie Yuheng <xyheme@gmail.com>
2022-08-04 15:18:43 +0800

    up

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,17 +1,10 @@
-<script setup lang="ts">
-// We can not use <NuxtLink> with headlessui:
+<script setup>
+// Maybe we can not use <RouterLink> in headlessui:
 // - https://headlessui.com/react/menu#integrating-with-next-js
-
-defineProps<{
-  href?: string
-  target?: string
-  class?: any
-  style?: any
-}>()
 </script>
 
 <template>
-  <RouterLink :to="href" :target="target" :class="class" :style="style">
+  <RouterLink :to="$attrs.href" v-bind="$attrs">
     <slot />
   </RouterLink>
 </template>
