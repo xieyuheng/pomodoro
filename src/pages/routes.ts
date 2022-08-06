62d26fad706cf2d40bb23941a7ecaecf5fdadfed
Xie Yuheng <xyheme@gmail.com>
2022-08-06 17:56:25 +0800

    - quit using `import()`

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -1,20 +1,22 @@
 import type { RouteRecordRaw } from "vue-router"
+import Pomodoro from "./pomodoro/Pomodoro.vue"
+import Login from "./login/Login.vue"
+import Register from "./register/Register.vue"
+import LoginEmailConfirmationSuccessPage from "./login/LoginEmailConfirmationSuccessPage.vue"
+import RegisterEmailConfirmationSuccessPage from "./register/RegisterEmailConfirmationSuccessPage.vue"
+import PageNotFound from "./errors/PageNotFound.vue"
 
 export const routes: Array<RouteRecordRaw> = [
-  { path: "/", component: () => import("./pomodoro/Pomodoro.vue") },
-  { path: "/login", component: () => import("./login/Login.vue") },
-  { path: "/register", component: () => import("./register/Register.vue") },
+  { path: "/", component: Pomodoro },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
   {
     path: "/notifications/login-email-confirmation-success",
-    component: () => import("./login/LoginEmailConfirmationSuccessPage.vue"),
+    component: LoginEmailConfirmationSuccessPage,
   },
   {
     path: "/notifications/register-email-confirmation-success",
-    component: () =>
-      import("./register/RegisterEmailConfirmationSuccessPage.vue"),
+    component: RegisterEmailConfirmationSuccessPage,
   },
-  {
-    path: "/:slugs(.*)*",
-    component: () => import("./errors/PageNotFound.vue"),
-  },
+  { path: "/:slugs(.*)*", component: PageNotFound },
 ]
