195015560435c4e1c269029a7bfd214c56757d07
Xie Yuheng <xyheme@gmail.com>
2022-08-08 09:08:09 +0800

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const colors = require("tailwindcss/colors")
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   safelist: [
