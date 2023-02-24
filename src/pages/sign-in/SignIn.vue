5dd8adf5b501a0d0254e414c7b2d7505478e13f1
Xie Yuheng <xyheme@gmail.com>
2023-02-25 06:30:47 +0800

    up

--- src/pages/sign-in/SignIn.vue
+++ src/pages/sign-in/SignIn.vue
@@ -1,23 +1,108 @@
 <script setup lang="ts">
+import {
+  ArrowRightCircleIcon,
+  EllipsisHorizontalCircleIcon,
+} from '@heroicons/vue/24/outline'
 import { reactive } from 'vue'
-import SignInStart from './SignInStart.vue'
-import SignInVerifying from './SignInVerifying.vue'
+import { useForm } from '../../components/form/useForm'
+import Hyperlink from '../../components/Hyperlink.vue'
+import Lang from '../../components/Lang.vue'
+import { useGlobalLang } from '../../reactives/useGlobalLang'
+import { useGlobalTheme } from '../../reactives/useGlobalTheme'
 import { State } from './State'
 
 const state = reactive(new State())
+
+const theme = useGlobalTheme()
+const lang = useGlobalLang()
+
+const form = useForm({ email: '' })
 
 state.auth.redirectUser()
 </script>
 
 <template>
-  <div
-    v-if="!state.verifying"
-    class="mt-4 flex h-full flex-col items-center md:mt-10"
-  >
-    <SignInStart :state="state" />
-  </div>
+  <div class="mt-4 flex h-full flex-col items-center md:mt-10">
+    <form
+      @submit.prevent="
+        form.post(state.links['sign-in'], {
+          then: (result) => state.verify(result),
+        })
+      "
+      class="flex w-full flex-col space-y-2 pt-20 text-xl sm:w-auto"
+    >
+      <div class="flex flex-col pb-2">
+        <div class="flex items-baseline justify-between">
+          <div class="font-logo text-3xl font-semibold">
+            <Lang>
+              <template #zh> 登录 </template>
+              <template #en> Sign in </template>
+            </Lang>
+          </div>
 
-  <div v-else class="mt-12 flex h-full flex-col items-center md:mt-10">
-    <SignInVerifying :state="state" :verifying="state.verifying" />
+          <div class="text-xl">
+            <Lang>
+              <template #zh>
+                尚未<Hyperlink href="/sign-up" class="underline">注册</Hyperlink
+                >？
+              </template>
+              <template #en>
+                Not
+                <Hyperlink href="/sign-up" class="underline"
+                  >signed up</Hyperlink
+                >
+                yet?
+              </template>
+            </Lang>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <div class="flex">
+          <input
+            v-model.trim="form.values.email"
+            id="email"
+            name="email"
+            autocomplete="email"
+            class="w-full rounded-sm border px-3 py-4 font-bold placeholder-opacity-60"
+            :class="[
+              `text-${theme.name}-700`,
+              `border-${theme.name}-600`,
+              `placeholder-${theme.name}-800`,
+            ]"
+            type="email"
+            maxlength="100"
+            :placeholder="lang.isZh() ? '电子邮箱' : 'Email'"
+            spellcheck="false"
+            required
+          />
+
+          <button
+            class="pl-2"
+            :class="[
+              form.processing
+                ? `text-${theme.name}-300`
+                : `text-${theme.name}-100`,
+            ]"
+            type="submit"
+            :disabled="form.processing"
+          >
+            <ArrowRightCircleIcon v-if="!form.processing" class="h-8 w-8" />
+            <EllipsisHorizontalCircleIcon
+              v-if="form.processing"
+              class="h-8 w-8"
+            />
+          </button>
+        </div>
+
+        <div v-if="form.response && !form.response.ok" class="mt-1">
+          <Lang class="py-1 text-base font-bold text-yellow-300">
+            <template #zh>这个邮箱不对</template>
+            <template #en>Invalid email.</template>
+          </Lang>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
