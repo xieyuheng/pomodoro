b65fc7034ec34a8c9a1de56d8373f840df1426ad
Xie Yuheng <xyheme@gmail.com>
2023-02-24 23:55:56 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,41 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import { VitePWA } from "vite-plugin-pwa"
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { host: "0.0.0.0" },
-  test: { environment: "jsdom" },
+  server: { host: '0.0.0.0' },
+  test: { environment: 'jsdom' },
   build: { sourcemap: true },
   plugins: [
     vue(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
       includeAssets: [
-        "/favicon.ico",
-        "/apple-touch-icon.png",
-        "/android-chrome-192x192.png",
-        "/android-chrome-512x512.png",
+        '/favicon.ico',
+        '/apple-touch-icon.png',
+        '/android-chrome-192x192.png',
+        '/android-chrome-512x512.png',
       ],
       manifest: {
-        name: "Pomodoro",
-        short_name: "Pomodoro",
-        description: "Pomodoro timer.",
-        display: "standalone",
-        start_url: "/",
-        theme_color: "#F87171",
-        background_color: "#F87171",
+        name: 'Pomodoro',
+        short_name: 'Pomodoro',
+        description: 'Pomodoro timer.',
+        display: 'standalone',
+        start_url: '/',
+        theme_color: '#F87171',
+        background_color: '#F87171',
         icons: [
           {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
