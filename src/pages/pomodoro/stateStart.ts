f5bdf5154f573fbc513d91eae454444350261528
Xie Yuheng <xyheme@gmail.com>
2023-03-02 08:24:43 +0800

    up

--- src/pages/pomodoro/stateStart.ts
+++ src/pages/pomodoro/stateStart.ts
@@ -11,6 +11,10 @@ export function stateStart(state: State): void {
     onFinished: async () => {
       if (state.tasks[0] && state.mode.kind === 'Focus') {
         state.tasks[0].trace.push(Date.now())
+        console.log({
+          who: '[start.timer / onFinished]',
+          task: state.tasks[0],
+        })
       }
 
       await stateNotify(state)
