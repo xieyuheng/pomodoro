f6d66a2496cf915194d96c72139f2f6c3f530378
Xie Yuheng <xyheme@gmail.com>
2023-03-05 17:15:46 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
@@ -18,37 +17,5 @@ export default defineConfig({
     //   - https://github.com/vitejs/vite/issues/2415
     //   - https://github.com/vitejs/vite/issues/4344
     pluginRewriteAll(),
-
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: { enabled: true },
-      includeAssets: [
-        '/favicon.ico',
-        '/apple-touch-icon.png',
-        '/android-chrome-192x192.png',
-        '/android-chrome-512x512.png',
-      ],
-      manifest: {
-        name: 'Pomodoro',
-        short_name: 'Pomodoro',
-        description: 'Pomodoro timer.',
-        display: 'standalone',
-        start_url: '/',
-        theme_color: '#F87171',
-        background_color: '#F87171',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
   ],
 })
