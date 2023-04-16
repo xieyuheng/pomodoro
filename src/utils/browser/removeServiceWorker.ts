f38f80cd42234e9b408a5635b3891ed082d17220
Xie Yuheng <xyheme@gmail.com>
2023-04-16 15:37:28 +0800

    removeServiceWorker

---
+++ src/utils/browser/removeServiceWorker.ts
@@ -0,0 +1,7 @@
+async function removeServiceWorker() {
+  const registrations = await navigator.serviceWorker.getRegistrations()
+
+  for (let registration of registrations) {
+    registration.unregister()
+  }
+}
