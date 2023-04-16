c4d90fe30a9bfe4f43032244f9f064b64d7f2b55
Xie Yuheng <xyheme@gmail.com>
2023-04-16 15:55:41 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 add pwa back -- notification need service worker
-`advance/` -- for uninstall service worker
 do not send login request when there is not token
 delete token when it is wrong
--- src/pages/settings/Settings.vue
+++ src/pages/settings/Settings.vue
@@ -7,15 +7,22 @@ import { removeServiceWorker } from '../../utils/browser/removeServiceWorker'
 <template>
   <PageLayout>
     <div class="mt-4 flex h-full flex-col items-center md:mt-10">
-      <button
-        @click="removeServiceWorker()"
-        class="rounded border-2 border-red-300 bg-red-500 p-3 text-xl"
-      >
-        <Lang>
-          <template #zh> 清除 Service Worker </template>
-          <template #en> Remove Service Worker </template>
+      <div class="flex flex-col space-y-4">
+        <Lang class="font-logo text-3xl font-semibold">
+          <template #zh>设置</template>
+          <template #en>Settings</template>
         </Lang>
-      </button>
+
+        <button
+          @click="removeServiceWorker()"
+          class="rounded-sm border-2 border-red-300 bg-red-500 p-3 text-xl"
+        >
+          <Lang>
+            <template #zh> 清除 Service Worker </template>
+            <template #en> Remove Service Worker </template>
+          </Lang>
+        </button>
+      </div>
     </div>
   </PageLayout>
 </template>
